c6be100a376feb1092cf8413169db52f53714748
dvdrtrgn <dvdrtrgn@gmail.com>
2015-12-16 10:22:46 -0600

    bring in libs

---
+++ app/scripts/libs/mailer.js
@@ -0,0 +1,101 @@
+/*jslint  white:false */
+/*globals define, window */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ created drt 2015-09
+
+ USE
+ prepare obj/json for sending to relay.php
+
+ TODO
+ document a bit
+ loosely load
+ */
+
+define(['jquery'], function
+    KLASS($) {
+    'use strict'; // semi-static closure
+
+//CLASS
+    var Nom = 'Mailer';
+    var W = (W && W.window || window), C = (W.C || W.console || {});
+    var Db = W.debug > 1;
+
+    var relay = 'http://ecgsolutions.hosting.wellsfargo.com/lib/relay.php';
+
+    return function Mailer(to, from, sub, msg, cc) {
+        this.to = to;
+        this.from = from;
+        this.sub = sub || 'Howdy';
+        this.msg = msg || 'howdy there pardner...' + new Date();
+        this.cc = cc || '';
+        this.key = '***';
+
+        this.setRelay = function (url) {
+            relay = url;
+        };
+        this.getRelay = function () {
+            return relay;
+        };
+        this.encodeObj = function () {
+            var key, str = '';
+
+            for (key in this) {
+                var val = this[key];
+
+                if (typeof val === 'function') {
+                    continue;
+                }
+                str += ('&' + key + '=');
+                str += encodeURIComponent(val.replace(/[\t\ ]+/g, ' '));
+            }
+            return str.slice(1);
+        };
+        this.preview = function () {
+            return this.getRelay() + '?' + this.encodeObj();
+        };
+        this.locate = function () {
+            W.location.href = this.preview();
+        };
+        this.get = function (cb) {
+            var test = $('<div>'),
+                text = this.preview();
+
+            test.appendTo('body').hide() //
+                .load(text, function (rez) {
+                    C.log(text, rez);
+                    cb();
+                    test.remove();
+                });
+        };
+        this.post = function (cb) {
+            $.ajax({
+                url: relay,
+                type: 'post',
+                datatype: 'json',
+                data: {
+                    to: this.to,
+                    sub: this.sub,
+                    from: this.from,
+                    cc: this.cc,
+                    msg: this.msg,
+                    key: this.key,
+                },
+                success: function (from) {
+                    C.debug('success', from);
+                    cb();
+                    // close relay
+                },
+                error: function () {
+                    // probably done but origin access prohibited
+                    C.debug('forced post', from);
+                    cb();
+                },
+            });
+        };
+    };
+});
+/*
+
+
+
+ */
