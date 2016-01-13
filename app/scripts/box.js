1da66eb10949a8fa9d3b94b9150e1cb4424c05b6
dvdrtrgn <dvdrtrgn@gmail.com>
2016-01-13 12:51:12 -0600

    document base types

--- app/scripts/box.js
+++ app/scripts/box.js
@@ -4,11 +4,19 @@
  created drt 2015-12
 
  USE
+    help manage two dimentional pairs
 
  TODO
+    expand upon methods
+    .area
+    .overlap
+    .collision
+    :proxyPoint? (set an anchor relative to top-left)
 
  */
 define(['pair'], function (Pair) {
+    'use strict';
+    // instance constructor
     function Box(w, h) {
         this.lt = new Pair(0, 0);
         this.rb = new Pair(w || 1, h || 1);
@@ -32,14 +40,17 @@ define(['pair'], function (Pair) {
         getHeight: function () {
             return Math.abs(this.rb.b - this.lt.b);
         },
+        // (toArray)
         getRect: function () {
             return [this.getLeft(), this.getTop(), this.getRight(), this.getBottom()];
         },
+        // adjust edges using default proxy point
         moveBy: function (n1, n2) {
             this.lt.moveBy(n1, n2);
             this.rb.moveBy(n1, n2);
             return this.getRect();
         },
+        // specify edges using default proxy point
         moveTo: function (n1, n2) {
             this.rb.moveBy(this.lt.diffWith(n1, n2));
             this.lt = new Pair(n1, n2);
